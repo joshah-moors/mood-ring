0722a3abf9b954b5eb501f6d25d8bd3b75eca056
Joshah <joshah.moors@utexas.edu>
2020-02-28 18:23:27 -0600

    Added context.py & renamed tests dir

--- test/test_mood_ring.py
+++
@@ -1,6 +0,0 @@
-from mood_ring import mood_map, Ring
-
-def test_ring_output_membership():
-    mood_list = [item[0] for item in mood_map]
-    mood_ring = Ring()
-    assert str(mood_ring) in mood_list
---
+++ tests/test_mood_ring.py
@@ -0,0 +1,6 @@
+from .context import mood_ring as mr
+
+def test_ring_output_membership():
+    mood_list = [item[0] for item in mr.mood_map]
+    mood_ring = mr.Ring()
+    assert str(mood_ring) in mood_list
